5c138fc77d77bbcc5bff6a17e5910a535ab689bd
talexcrowell <talexcrowell@gmail.com>
2018-12-19 09:31:07 -0800

    react app taking shape

--- searching-drills/src/App.js
+++ searching-drills/src/App.js
@@ -1,25 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import DatasetInput from './dataset-input';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    
+    this.state ={
+      searchArr : [89,30,25,32,72,70,51,42,25,24],
+      counter: 0,
+      value: null
+    };
+  }
+
+  addDataset(value){
+    this.setState({
+      value
+    });
+  }
+
+  linearSearch(arr, value){
+    let counter =0;
+    for(let i =0; i<arr.length; i++){
+      counter ++;
+      if(value === arr[i]){
+        return `We found ${value} after ${counter} tries using linear search`
+      }
+    }
+    return `We could not find ${value}`
+  }
+
+
+
+  binarySearch(arr, value, start, end){
+    const sortedArray = arr.sort();
+    if(start>end){
+      return `Could not find ${value} in dataset`
+    }
+    let index = Math.floor((start+end)/2);
+    let item = sortedArray[index];
+
+    if(item === value){
+      return index;
+    }
+
+    else if(item < value){
+      return binarySearch(sortedArray, value, index+1, end);
+    }
+    else if(item > value){
+      return binarySearch(sortedArray, value, start, index-1);
+    }
+
+  }
+
   render() {
+
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <main>
+          <DatasetInput />
+        </main>
       </div>
     );
   }
---
+++ searching-drills/src/dataset-input.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function DatasetInput(props){
+    return(
+      <form>
+        <label>Enter your dataset below:</label>
+        <input type="text" placeholder="Enter your data here..."></input>
+        <button>Linear Search</button>
+
+        <button>Binary Search</button>
+      </form>);
+  
+};
+
+
